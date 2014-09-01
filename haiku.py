565f7b20da9485e8f16a5d29da78cc4afe3376a9
Dean Shaff <dshaff001@gmail.com>
2014-09-01 14:59:10 +0400

    Added twitter support. authorization codes are encrypted

--- haiku.py
+++ haiku.py
@@ -5,6 +5,9 @@ import numpy.random as random
 import urllib2
 from bs4 import BeautifulSoup as bs 
 import time
+from tweepy_tools import Twitter_Tools
+from tweepy_tools import Article_Stuff
+
 
 text = """
 agreement with the Emirate of Abu Dhabi to create NYU Abu Dhabi 
@@ -32,6 +35,14 @@ NYU Abu Dhabi and NYU New York form the backbone of a fully connected global net
 As one of the two major hubs in the global network, NYUAD creates a unique capacity for faculty and students to access the assets of the entire university system.
 NYU Abu Dhabi advances the city of Abu Dhabi as a magnetic center of ideas and human talent.
 """
+
+text1 = str()
+articles = Article_Stuff()
+title_list = articles.gen_article_titles()
+for i in title_list:
+	text1 += i
+
+print(text1)
 
 def find_syll(word): #VERY specific to the howmanysyllables.com website
 	t1 = time.time()
@@ -84,10 +95,10 @@ def make_syll(): #Now I can use both sources! From the internet from the nltk
 			print(time1)
 	return wording
 
-word_list1 = make_syll()
-print(word_list1)
+# word_list1 = make_syll()
+# print(word_list1)
 
-word_list2 = [['agreement ', 3], ['with ', 1], ['the ', 1], ['Emirate ', 3], ['of ', 1], ['Abu ', 2], ['Dhabi', 2], ['to ', 1], ['create ', 2], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['is ', 1], ['the ', 1], ['outcome ', 2], ['of ', 1], ['a ', 1], ['shared ', 1], ['understanding ', 4], ['of ', 1], ['the ', 1], ['essential ', 3], ['roles ', 1], ['and ', 1], ['challenges ', 3], ['of ', 1], ['higher ', 2], ['education ', 4], ['in ', 1], ['the ', 1], ['century ', 3], ['a ', 1], ['common ', 2], ['belief ', 2], ['in ', 1], ['value ', 2], ['of ', 1], ['a ', 1], ['liberal ', 3], ['arts ', 1], ['education ', 4], ['concurrence ', 3], ['on ', 1], ['the ', 1], ['benefits ', 3], ['a ', 1], ['research ', 2], ['university ', 5], ['brings ', 1], ['to ', 1], ['the ', 1], ['society ', 4], ['that ', 1], ['sustains ', 2], ['it ', 1], ['a ', 1], ['conviction ', 3], ['that ', 1], ['interaction ', 4], ['with ', 1], ['new ', 1], ['ideas ', 3], ['and ', 1], ['people ', 2], ['who ', 1], ['are ', 1], ['different ', 3], ['is ', 1], ['valuable ', 4], ['and ', 1], ['necessary ', 4], ['and ', 1], ['a ', 1], ['commitment ', 3], ['to ', 1], ['educating ', 4], ['students ', 2], ['who ', 1], ['are ', 1], ['true ', 1], ['citizens ', 3], ['of ', 1], ['the ', 1], ['As ', 1], ['the ', 1], ['first ', 1], ['comprehensive ', 4], ['liberal ', 3], ['arts ', 1], ['and ', 1], ['science ', 2], ['campus ', 2], ['in ', 1], ['the ', 1], ['Middle ', 2], ['East ', 1], ['to ', 1], ['be ', 1], ['operated ', 4], ['abroad ', 2], ['by ', 1], ['a ', 1], ['major ', 2], ['American ', 4], ['research ', 2], ['university ', 5], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['has ', 1], ['been ', 1], ['built ', 1], ['on ', 1], ['the ', 1], ['following ', 3], ['principles ', 3], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['is ', 1], ['a ', 1], ['research ', 2], ['university ', 5], ['with ', 1], ['a ', 1], ['fully ', 2], ['integrated ', 4], ['liberal ', 3], ['arts ', 1], ['and ', 1], ['science ', 2], ['It ', 1], ['draws ', 1], ['students ', 2], ['from ', 1], ['around ', 2], ['the ', 1], ['world ', 1], ['and ', 1], ['prepares ', 2], ['them ', 1], ['for ', 1], ['the ', 1], ['challenges ', 3], ['and ', 1], ['opportunities ', 5], ['of ', 1], ['our ', 1], ['interconnected ', 5], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['equips ', 2], ['students ', 2], ['for ', 1], ['leadership ', 3], ['in ', 1], ['all ', 1], ['arenas ', 3], ['of ', 1], ['human ', 2], ['endeavor ', 3]]
+word_list2 = [['agreement ', 3], ['with ', 1], ['the ', 1], ['Emirate ', 3], ['of ', 1], ['Abu ', 2], ['Dhabi ', 2], ['to ', 1], ['create ', 2], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['is ', 1], ['the ', 1], ['outcome ', 2], ['of ', 1], ['a ', 1], ['shared ', 1], ['understanding ', 4], ['of ', 1], ['the ', 1], ['essential ', 3], ['roles ', 1], ['and ', 1], ['challenges ', 3], ['of ', 1], ['higher ', 2], ['education ', 4], ['in ', 1], ['the ', 1], ['century ', 3], ['a ', 1], ['common ', 2], ['belief ', 2], ['in ', 1], ['value ', 2], ['of ', 1], ['a ', 1], ['liberal ', 3], ['arts ', 1], ['education ', 4], ['concurrence ', 3], ['on ', 1], ['the ', 1], ['benefits ', 3], ['a ', 1], ['research ', 2], ['university ', 5], ['brings ', 1], ['to ', 1], ['the ', 1], ['society ', 4], ['that ', 1], ['sustains ', 2], ['it ', 1], ['a ', 1], ['conviction ', 3], ['that ', 1], ['interaction ', 4], ['with ', 1], ['new ', 1], ['ideas ', 3], ['and ', 1], ['people ', 2], ['who ', 1], ['are ', 1], ['different ', 3], ['is ', 1], ['valuable ', 4], ['and ', 1], ['necessary ', 4], ['and ', 1], ['a ', 1], ['commitment ', 3], ['to ', 1], ['educating ', 4], ['students ', 2], ['who ', 1], ['are ', 1], ['true ', 1], ['citizens ', 3], ['of ', 1], ['the ', 1], ['As ', 1], ['the ', 1], ['first ', 1], ['comprehensive ', 4], ['liberal ', 3], ['arts ', 1], ['and ', 1], ['science ', 2], ['campus ', 2], ['in ', 1], ['the ', 1], ['Middle ', 2], ['East ', 1], ['to ', 1], ['be ', 1], ['operated ', 4], ['abroad ', 2], ['by ', 1], ['a ', 1], ['major ', 2], ['American ', 4], ['research ', 2], ['university ', 5], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['has ', 1], ['been ', 1], ['built ', 1], ['on ', 1], ['the ', 1], ['following ', 3], ['principles ', 3], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['is ', 1], ['a ', 1], ['research ', 2], ['university ', 5], ['with ', 1], ['a ', 1], ['fully ', 2], ['integrated ', 4], ['liberal ', 3], ['arts ', 1], ['and ', 1], ['science ', 2], ['It ', 1], ['draws ', 1], ['students ', 2], ['from ', 1], ['around ', 2], ['the ', 1], ['world ', 1], ['and ', 1], ['prepares ', 2], ['them ', 1], ['for ', 1], ['the ', 1], ['challenges ', 3], ['and ', 1], ['opportunities ', 5], ['of ', 1], ['our ', 1], ['interconnected ', 5], ['NYU', 1], ['Abu ', 2], ['Dhabi', 2], ['equips ', 2], ['students ', 2], ['for ', 1], ['leadership ', 3], ['in ', 1], ['all ', 1], ['arenas ', 3], ['of ', 1], ['human ', 2], ['endeavor ', 3]]
 
 
 # wordinglist1 = [['agreement ', 3], ['with ', 1], ['the ', 1], ['Emirate ', 3], ['of ', 1], ['Abu ', 2], ['to ', 1], ['create ', 2], ['Abu ', 2], ['is ', 1], ['the ', 1], ['outcome ', 2], ['of ', 1], ['a ', 1], ['shared ', 1], ['understanding ', 4], ['of ', 1], ['the ', 1], ['essential ', 3]]
@@ -96,9 +107,10 @@ def make_poem(wordlist): #wordlist should be a list that looks like what make_sy
 	poem = str()
 	wordlist = list(wordlist)
 	syll_list = [5,7,5]
+
 	def make_line(syll):
 		integer_list = random.choice(xrange(len(wordlist)),size=len(wordlist),replace=False)
-		line = str()		
+		line = [str(),0]		
 		for j in xrange(len(integer_list)-2):
 			i = integer_list[j]
 			iplus1 = integer_list[j+1]
@@ -106,18 +118,26 @@ def make_poem(wordlist): #wordlist should be a list that looks like what make_sy
 			item_next = wordlist[iplus1]
 			if item[0] == item_next[0]:
 				pass
-			if syllables_en.count(line) + item[1] > syll:
+			if line[1] + item[1] > syll:
 				pass
-			elif syllables_en.count(line) + item[1] == syll:
-				return str(line)
-			elif syllables_en.count(line) + item[1] < syll:
-				line += item[0]
+			elif line[1] + item[1] == syll:
+				return {'line': str(line[0]), 'line_syll': int(line[1])}
+			elif line[1] + item[1] < syll:
+				line[0] += item[0]
+				line[1] += item[1]
+
+	#checking to see if the syllables are correct			
 	for i in syll_list:
-		line = str(make_line(i+1))
+		line_info = make_line(i+1)
+		line = line_info['line']
+		line_num = line_info['line_syll']
 		while int(syllables_en.count(line)) < int(i):
 			line = str(make_line(i+1))
 		poem = poem+line+'\n'
 
+	while syllables_en.count(poem) != 17:
+		poem = make_poem(wordlist)
+
 	return poem 
 
-print(make_poem(word_list1))
+# print(make_poem(word_list2))
